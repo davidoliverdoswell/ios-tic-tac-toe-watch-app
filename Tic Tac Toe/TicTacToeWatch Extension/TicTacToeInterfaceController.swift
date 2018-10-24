f9559011d15a5a27ad0ad7703e167b3be90c899c
David O. Doswell <daviddoswell@lambda-school-loaner-11s-macbook-pro.local>
2018-10-24 15:24:00 -0700

    set up groups, outlets, and actions

--- Tic Tac Toe/TicTacToeWatch Extension/TicTacToeInterfaceController.swift
+++ Tic Tac Toe/TicTacToeWatch Extension/TicTacToeInterfaceController.swift
@@ -11,9 +11,62 @@ import Foundation
 
 
 class TicTacToeInterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet weak var reset: WKInterfaceButton!
+    
+    @IBOutlet weak var playerXOLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var firstXOOutlet: WKInterfaceButton!
+    
+    @IBOutlet weak var secondXOOutlet: WKInterfaceButton!
+    
+    @IBOutlet weak var thirdXOOutlet: WKInterfaceButton!
+    
+    @IBOutlet weak var fourthXOOutlet: WKInterfaceButton!
+    
+    @IBOutlet weak var fifthXOOutlet: WKInterfaceButton!
+    
+    @IBOutlet weak var sixthXOOutlet: WKInterfaceButton!
+    
+    @IBOutlet weak var seventhXOOutlet: WKInterfaceButton!
+    
+    @IBOutlet weak var eighthXOOutlet: WKInterfaceButton!
+    
+    @IBOutlet weak var ninthXOOutlet: WKInterfaceButton!
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
     }
+    
+    @IBAction func resetGame() {
+    }
+    
+    @IBAction func firstXOTap() {
+    }
+    
+    @IBAction func secondXOTap() {
+    }
+    
+    @IBAction func thirdXOTap() {
+    }
+    
+    @IBAction func fourthXOTap() {
+    }
+    
+    @IBAction func fifthXOTap() {
+    }
+    
+    @IBAction func sixthXOTap() {
+    }
+    
+    @IBAction func seventhXOTap() {
+    }
+    
+    @IBAction func eighthXOTap() {
+    }
+    
+    @IBAction func ninthXOTap() {
+    }
+    
 }
