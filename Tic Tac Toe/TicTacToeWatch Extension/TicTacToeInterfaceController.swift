f54a4bd5b8f44d2107f898fde637bd00957e29b9
David O. Doswell <daviddoswell@lambda-school-loaner-11s-macbook-pro.local>
2018-10-24 14:45:38 -0700

    set up targets and group

---
+++ Tic Tac Toe/TicTacToeWatch Extension/TicTacToeInterfaceController.swift
@@ -0,0 +1,19 @@
+//
+//  TicTacToeInterfaceController.swift
+//  TicTacToeWatch Extension
+//
+//  Created by David Doswell on 10/24/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class TicTacToeInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+    }
+}
